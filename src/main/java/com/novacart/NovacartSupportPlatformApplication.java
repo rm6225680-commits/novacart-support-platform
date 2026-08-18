cdf523de02ec507ee6b18b260068384f78346fbd
Ashu <rm6225680@gmail.com>
2026-08-18 14:58:07 +0530

    chore: initial spring boot project setup from STS

---
+++ src/main/java/com/novacart/NovacartSupportPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.novacart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovacartSupportPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovacartSupportPlatformApplication.class, args);
+	}
+
+}
